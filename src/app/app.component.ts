82a4a7e0abc45ef3c91d1af46712ea70d0cb23ac
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2022-11-03 17:37:00 -0500

    18. Interpolation
    Iniciando a seção 04 - Template Data Binding Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,43 +3,17 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 @Component({
   selector: 'app-root',
   template: `
-    <app-title *ngIf="destruir"></app-title>
-    <br>
-    <button (click)="destruirComponent()">Destruir componente</button>
+    <app-data-binding></app-data-binding>
     <router-outlet></router-outlet>
   `
 })
-export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked{
-  
-  public destruir: boolean = true;
+export class AppComponent implements OnInit { 
   
   constructor() {}
 
-  public destruirComponent() {
-    this.destruir = false;
-  }
 
   ngOnInit(): void {
   }
 
-  ngDoCheck(): void {
-    console.log("ngDoCheck");      
-  }
-  
-  ngAfterContentInit(): void {
-    console.log("ngAfterContentInit");      
-  }
-  
-  ngAfterContentChecked(): void {
-    console.log("ngAfterContentChecked");      
-  }
-  
-  ngAfterViewInit(): void {
-    console.log("ngAfterViewInit");      
-  }
-  
-  ngAfterViewChecked(): void {
-    console.log("ngAfterViewChecked");      
-  }
 
 }
---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss']
+})
+export class DataBindingComponent implements OnInit {
+
+  public nome: string = "Hermann";
+
+  public idade: number = 29; 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
