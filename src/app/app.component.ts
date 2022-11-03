d19bdfce34b8b2bf115e14d75bfb5424a6d4c181
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2022-11-03 16:39:50 -0500

    15. ngOnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,20 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 @Component({
   selector: 'app-root',
   template: `
-    {{ valor }}
-    <button (click)="adicionar()">Adicionar</button>
+    <app-title *ngIf="destruir"></app-title>
+    <br>
+    <button (click)="destruirComponent()">Destruir componente</button>
     <router-outlet></router-outlet>
-    `
+  `
 })
 export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked{
   
-  public valor: number = 1;
+  public destruir: boolean = true;
   
   constructor() {}
 
-  public adicionar(): number {
-    return this.valor += 1;
+  public destruirComponent() {
+    this.destruir = false;
   }
 
   ngOnInit(): void {
