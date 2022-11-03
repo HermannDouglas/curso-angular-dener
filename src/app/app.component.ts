57849e680e425293b2b7908ec6d4b3ef0dbef695
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2022-11-03 16:10:33 -0500

    15. ngDoCheck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <app-title></app-title>
+    {{ valor }}
+    <button (click)="adicionar()">Adicionar</button>
     <router-outlet></router-outlet>
     `
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked{
+  
+  public valor: number = 1;
+  
   constructor() {}
+
+  public adicionar(): number {
+    return this.valor += 1;
+  }
 
   ngOnInit(): void {
   }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck");      
+  }
   
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit");      
+  }
+  
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked");      
+  }
+  
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");      
+  }
+  
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked");      
+  }
+
 }
