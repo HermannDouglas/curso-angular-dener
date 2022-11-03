b003ee0cc0f4d455a1312f692ca98e989db03e2f
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2022-11-03 18:05:42 -0500

    19. Property Binding - pt. 01

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -10,7 +10,9 @@ export class DataBindingComponent implements OnInit {
   public nome: string = "Hermann";
   public idade: number = 29; 
   public maisUm: number = 1;
-  
+
+  public checkedDisabled: boolean = true;
+  public imgSrc: string = "https://vidafullstack.com.br/wp-content/uploads/2021/12/banner.jpg";
 
   constructor() { }
 
