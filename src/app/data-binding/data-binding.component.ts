084f69460b88d0fae0342579979caa49ff7fc2a9
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2022-11-10 16:01:31 -0500

    20. Event Binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -13,10 +13,20 @@ export class DataBindingComponent implements OnInit {
 
   public checkedDisabled: boolean = true; 
   public imgSrc: string = "https://vidafullstack.com.br/wp-content/uploads/2021/12/banner.jpg";
+  public imgTitle: string = "Property Binding";
 
+  public position: { x: number, y: number } = { x: 0, y: 0 };
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  }
+
+  public alertaInfo(valor: MouseEvent) {
+    console.log(valor);
+  }
+
+  public mouseMoveTeste(valor: MouseEvent) {
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
   }
 
 }
