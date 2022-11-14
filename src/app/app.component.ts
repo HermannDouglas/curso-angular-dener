be2d64a4fe94cfe6d219c093611e65d3416d5e46
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2022-11-14 15:50:09 -0500

    23. *ngIf (Parcial 1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 @Component({
   selector: 'app-root',
   template: `
+    <app-diretivas-estruturais></app-diretivas-estruturais>
     <app-data-binding></app-data-binding>
     <router-outlet></router-outlet>
   `
